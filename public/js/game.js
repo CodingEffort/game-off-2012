030980094508808277bea049562325b5c37fa723
Jesse Emond <emond.jesse@gmail.com>
2012-11-09 01:33:29 -0500

    Can now shoot fireballs
    IMMA FIRIN' MY PEW PEW PEW PEW LAZORSSS.

--- public/js/game.js
+++ public/js/game.js
@@ -8,7 +8,8 @@ Crafty.init(SCREEN_W, SCREEN_H);
 // Load the spritesheet
 Crafty.sprite(50, "assets/back.png", {
 	space: [0,0, 16, 12], // the space background
-	ship: [0, 12] // the spaceship
+	ship: [0, 12], // the spaceship
+	fireball: [1, 12] // the fireball
 	});
 	
 // Represents an image that will reset up the screen once it reached the bottom
@@ -24,6 +25,20 @@ Crafty.c("ScreenScrolldown", {
 	}
 });
 
+// Makes a component go up until it reaches the top of the screen, then disapears
+Crafty.c("GoUp", {
+	init: function() {
+		this.bind("EnterFrame", function() {
+			this.y -= 10;
+			if (this.y + this.height < 0)
+				this.destroy();
+		});
+	}
+});
+
+
+// Makes the player go towards the mouse when the mouse moves on this
+// component
 Crafty.c("MakePlayerMoveOnMouseMove", {
 	init: function() {
 		this.requires("Mouse");
@@ -32,17 +47,23 @@ Crafty.c("MakePlayerMoveOnMouseMove", {
 			player.requires("Tween");
 			var targetX = Crafty.math.clamp(e.x - 30, 0, SCREEN_W - 50);
 			var targetY = Crafty.math.clamp(e.y - 30, 0, SCREEN_H - 50);
-			player.tween({x: targetX, y: targetY}, 3);
+			player.x = targetX;
+			player.y = targetY;
+		});
+
+		// Check to fire for the player.
+		this.bind("Click", function(e) {
+			Crafty.e("2D, Canvas, fireball, GoUp").attr({x: player.x, y: player.y});
 		});
 	}
 });
 
+
+// Main spaceship object
 Crafty.c("Spaceship", {
 	init: function() {
 	}
-
 });
-
 
 // Create an infinite background illusion with 2 images moving
 Crafty.e("2D, Canvas, space, ScreenScrolldown, MakePlayerMoveOnMouseMove");
