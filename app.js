b441fc92ee7065ad5001236673def6a960f5fa3e
William Turner <willtur.will@gmail.com>
2012-11-09 01:40:47 -0500

    Just realised how stupid it is to pass the whole node_modules directory around..... :|

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    cons = require('consolidate'),
+    db = require('./models');
+
+var config = require('./config');
+
+var RedisStore = (config.redisStore) ? require('connect-redis')(express) : null;
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.engine('hjs', cons.hogan);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'hjs');
+  app.locals({ title: config.title || '', subtitle: config.subtitle || '', prefix: config.prefix || '' });
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
+  app.use(express.cookieParser(config.secret));
+  if (config.redisStore)
+    app.use(express.session({ secret: config.secret, store: new RedisStore({ host: config.redis.host || 'localhost', port: config.redis.port || 6379 }) }));
+  else
+    app.use(express.session({ secret: config.secret }));
+  app.use(function(req, res, next) {
+    res.locals.user = req.session.user;
+    next();
+  });
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -29,9 +40,9 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+// http://madhums.me/2012/07/19/breaking-down-app-js-file-nodejs-express-mongoose/
+require('./controllers')(app, db, config.prefix);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+http.createServer(app).listen(config.port || 3000, config.host || '0.0.0.0', function() {
+  console.log("Project Nixie worker listening on " + (config.host || '0.0.0.0') + ":" + (config.port || 3000));
 });
