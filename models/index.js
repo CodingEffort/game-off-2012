f519b4d0a7964f495f44b4c6d3912400b990806f
William Turner <willtur.will@gmail.com>
2012-11-12 11:53:39 -0500

    Fixed the MongoDB createConnection and added a basic use model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,14 @@
 var config = require('../config');
 
 var mongoose = require('mongoose'),
-    mongo    = mongoose.createConnection(config.mongo.host || '127.0.0.1', config.mongo.port || 27017),
+    mongo    = mongoose.createConnection(config.mongo.host || '127.0.0.1', config.mongo.db, config.mongo.port || 27017),
     Schema   = mongoose.Schema;
+
+var User = new Schema({
+  username : { type: String, index: { unique: true } },
+  password : String,
+  email    : { type: String, index: { unique: true } }
+});
+
+module.exports.user = mongo.model('user', User);
 
